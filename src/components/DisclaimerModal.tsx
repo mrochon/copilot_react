e44ae6c249530a2ac6699468302469395f7fb914
mrochon <sailingrock@live.com>
2026-01-28 11:27:41 -0800

    Neutral logo

--- src/components/DisclaimerModal.tsx
+++ src/components/DisclaimerModal.tsx
@@ -11,7 +11,7 @@ export const DisclaimerModal: React.FC<DisclaimerModalProps> = ({ onContinue }) 
       <div className="disclaimer-modal">
         <div className="disclaimer-content">
           <h2>{import.meta.env.VITE_APP_DISCLAIMER_TITLE}</h2>
-          <img src="/Ask_Blair.png" alt="Ask Blair" className="disclaimer-image" />
+          <img src="/logo.png" alt="Ask me" className="disclaimer-image" />
           <p className="disclaimer-message">
             {import.meta.env.VITE_APP_DISCLAIMER_MESSAGE}</p>
           <p className="disclaimer-message">Press Continue to acknowledge.</p>
